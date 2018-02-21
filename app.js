1d7d4a984d91f83c126fc012d4ca41b9b099a732
Jerehme Gayle <jerehme.gayle@gmail.com>
2018-02-20 18:33:17 -0700

    Got the clear tasks working, clearing individual tasks by X now works, Added filtering capability

--- app.js
+++ app.js
@@ -12,9 +12,16 @@ loadEventListeners();
 function loadEventListeners(){
     // ADD TASK EVEN
     form.addEventListener('submit', addTask);
+    // REMOVE TASK EVENT
+    taskList.addEventListener('click', removeTask);
+    // CLEAR LIST EVENT
+    clearBtn.addEventListener('click', clearTasks);
+    // FILTER THROUGH TASKS
+    filter.addEventListener('keyup', filterTasks);
 }
 
-// ADD TASK
+//  ADD TASK
+
 function addTask(e){
     // CHECK TO SEE IF THERE IS A VALUE IN INPUT APON SUBMIT
     if(taskInput.value === ''){
@@ -40,4 +47,42 @@ function addTask(e){
     taskInput.value = '';
 
     e.preventDefault();
+}
+
+// REMOVE TASK
+
+function removeTask(e){
+  if(e.target.parentElement.classList.contains('delete-item')) {
+    if(confirm('Are you sure?')){
+      e.target.parentElement.parentElement.remove();
+    }
+  }
+}
+
+// CLEAR TASKS
+
+function clearTasks(){
+  // ONE WAY TO CLEAR TASKS
+  // taskList.innerHTML = '';
+  // FASTER WAY TO CLEAR TASKS
+  while(taskList.firstChild){
+    taskList.removeChild(taskList.firstChild);
+  }
+}
+
+// FILTER TASKS 
+
+function filterTasks(e){
+  const text = e.target.value.toLowerCase();
+
+  document.querySelectorAll('.collection-item').forEach
+  (function(task){
+    const item = task.firstChild.textContent;
+    if(item.toLowerCase().indexOf(text) != -1){
+      task.style.display = 'block';
+    } else {
+      task.style.display = 'none';
+    }
+  });
+
 }
